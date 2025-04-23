fcb3a3c23d13d4776ed32bdd2794cd327645ebc7
pdcxs <pdcxs@outlook.com>
2025-04-23 17:59:31 +0800

    Add project configuration for demo

--- demo/basic/src/TypeAutoConvertion.java
+++ demo/basic/src/TypeAutoConvertion.java
@@ -10,8 +10,8 @@ public class TypeAutoConvertion {
     public static void main(String[] args) {
         int a = 155;
         float b = 21.0f;
-        System.out.println("a=" + a + ",b=" + b);
-        System.out.println("a/b=" + (a / b));
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a / b = " + (a / b));
         System.out.println("1 / 3 = " + 1 / 3);
         System.out.println("1 / 3.0 = " + 1 / 3.0);
     }
